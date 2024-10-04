ecdad15509d9196ab54aec72dde1953ccb547141
jps-ibms <146703982+jpingsu@users.noreply.github.com>
2024-10-04 17:45:40 +0800

    Update remove_MHC.py

--- 4.3-Heritability/remove_MHC.py
+++ 4.3-Heritability/remove_MHC.py
@@ -8,5 +8,5 @@ df = pd.read_table(f'summary/{phe}_gwas.pheno.glm.logistic')
 df['chi square'] = df['Z_STAT'] * df['Z_STAT']
 df = df[df['chi square'] <= 80]
 #MHC region
-df_filtered = df[~((df['#CHROM'] == 6) & (df['POS'] >= 25000000) & (df['POS'] <= 36000000))]
+df_filtered = df[~((df['#CHROM'] == 6) & (df['POS'] >= 25391792) & (df['POS'] <= 33424245) )]
 df_filtered.to_csv(f'formatted/{phe}_qc.summary',index=None, sep='\t')
