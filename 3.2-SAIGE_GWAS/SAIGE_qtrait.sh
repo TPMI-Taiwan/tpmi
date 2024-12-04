4494846e34eef9efb8e94417e5a792d08e2e0281
wanjia-ibms <146334869+wanjia0360@users.noreply.github.com>
2024-12-04 16:09:53 +0800

    Add files via upload

--- 3.2-SAIGE_GWAS/SAIGE_qtrait.sh
+++ 3.2-SAIGE_GWAS/SAIGE_qtrait.sh
@@ -9,13 +9,12 @@ Rscrip step1_fitNULLGLMM.R \
 	--plinkFile=../binary_fileset/tpmi037 \
 	--traitType=quantitative \
 	--phenoFile=pheno_cov.txt \
-	--phenoCol=pheno \
-	--covarColList=gender_code,age_standardize,age_square_standardize,age_gender,age_square_gender,site,array,log10_records,PCAiR1,PCAiR2,PCAiR3,PCAiR4,PCAiR5,PCAiR6,PCAiR7,PCAiR8,PCAiR9,PCAiR10 \
-	--qCovarColList=gender_code,site,array \
+	--phenoCol=residual_int \
+	--covarColList=array,PCAiR1,PCAiR2,PCAiR3,PCAiR4,PCAiR5,PCAiR6,PCAiR7,PCAiR8,PCAiR9,PCAiR10 \
+	--qCovarColList=array \
 	--sampleIDColinphenoFile=IID \
 	--IsOverwriteVarianceRatioFile=TRUE \
 	--outputPrefix=${trait}_step1 \
-	--invNormalize=TRUE \
 	--LOCO=FALSE \
 	--nThreads=128 
 
